de33319bab8dba7d9c8c97c773b9cf9d3ba6548f
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-27 21:42:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
-end
+def fizzbuzz(int)
+  if  int % 3 == 0
+    puts "fizz"
+  elsif int % 5 == 0
+    puts "buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    puts "fizzbuzz"
+  else puts "nill"
+  end
+  end
